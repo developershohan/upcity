1f216782ac7ca34feb2946c89341a3671c1af7ae
Shohan <dev.shohanur@gmail.com>
2024-06-29 16:30:46 +0600

    need to work on cart

---
+++ src/features/cart/cartApiSlice.jsx
@@ -0,0 +1,9 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export const addToCart = createAsyncThunk(cart / addToCart, async (data) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+});
