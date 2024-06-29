1361af377da7ad35c54baadfd8ad50dc7b142987
shohan <dev.shohanur@gmail.com>
2024-06-30 00:10:20 +0600

    need to work with cart

--- src/features/cart/cartApiSlice.jsx
+++ src/features/cart/cartApiSlice.jsx
@@ -8,3 +8,11 @@ export const addToCart = createAsyncThunk("cart/addToCart", async (data) => {
     throw new Error(error.message);
   }
 });
+export const findItemsByUserId = createAsyncThunk("cart/findItemsByUserId", async (userId) => {
+  try {
+    const response = await axios.post(`http://localhost:8080/cart?user=${userId}`,userId);
+    return response.data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+});
